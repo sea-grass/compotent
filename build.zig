7122983bcd5ea3a3eca1a5efa49d65b0bd71595f
sea-grass <dev.seagrass@gmail.com>
2025-04-19 16:44:04 -0400

    Add example component rendering

--- build.zig
+++ build.zig
@@ -1,100 +1,63 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    // This creates a "module", which represents a collection of source files alongside
-    // some compilation options, such as optimization mode and linked system libraries.
-    // Every executable or library we compile will be based on one or more modules.
+    const quickjs_dep = b.dependency("quickjs", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const c_mod = CModule.build(b, .{
+        .quickjs = quickjs_dep,
+        .target = target,
+        .optimize = optimize,
+    });
+
     const lib_mod = b.createModule(.{
-        // `root_source_file` is the Zig "entry point" of the module. If a module
-        // only contains e.g. external object files, you can make this `null`.
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    // We will also create a module for our other entry point, 'main.zig'.
+    lib_mod.addImport("c", c_mod);
+
     const exe_mod = b.createModule(.{
-        // `root_source_file` is the Zig "entry point" of the module. If a module
-        // only contains e.g. external object files, you can make this `null`.
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    // Modules can depend on one another using the `std.Build.Module.addImport` function.
-    // This is what allows Zig source code to use `@import("foo")` where 'foo' is not a
-    // file path. In this case, we set up `exe_mod` to import `lib_mod`.
     exe_mod.addImport("compotent_lib", lib_mod);
+    exe_mod.addImport("c", c_mod);
 
-    // Now, we will create a static library based on the module we created above.
-    // This creates a `std.Build.Step.Compile`, which is the build step responsible
-    // for actually invoking the compiler.
     const lib = b.addLibrary(.{
         .linkage = .static,
         .name = "compotent",
         .root_module = lib_mod,
     });
 
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
     b.installArtifact(lib);
 
-    // This creates another `std.Build.Step.Compile`, but this one builds an executable
-    // rather than a static library.
     const exe = b.addExecutable(.{
         .name = "compotent",
         .root_module = exe_mod,
     });
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
     b.installArtifact(exe);
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
     const run_cmd = b.addRunArtifact(exe);
 
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
     run_cmd.step.dependOn(b.getInstallStep());
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
     const lib_unit_tests = b.addTest(.{
         .root_module = lib_mod,
     });
@@ -107,10 +70,46 @@ pub fn build(b: *std.Build) void {
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
 }
+
+const CModule = struct {
+    const header_bytes = (
+        \\#include <quickjs.h>
+        \\#include <quickjs-libc.h>
+    );
+
+    pub const Options = struct {
+        quickjs: *std.Build.Dependency,
+        target: std.Build.ResolvedTarget,
+        optimize: std.builtin.OptimizeMode,
+    };
+
+    pub fn build(b: *std.Build, opts: Options) *std.Build.Module {
+        const c = translateC(b, opts);
+        const mod = createModule(c, opts);
+        return mod;
+    }
+
+    fn translateC(b: *std.Build, opts: Options) *std.Build.Step.TranslateC {
+        const header_file = b.addWriteFiles().add("c.h", header_bytes);
+
+        const c = b.addTranslateC(.{
+            .root_source_file = header_file,
+            .target = opts.target,
+            .optimize = opts.optimize,
+            .link_libc = true,
+        });
+        c.addIncludePath(opts.quickjs.artifact("quickjs").getEmittedIncludeTree());
+
+        return c;
+    }
+
+    fn createModule(c: *std.Build.Step.TranslateC, opts: Options) *std.Build.Module {
+        const mod = c.createModule();
+        mod.linkLibrary(opts.quickjs.artifact("quickjs"));
+        return mod;
+    }
+};
--- src/root.zig
+++ src/root.zig
@@ -1,13 +1,143 @@
-//! By convention, root.zig is the root source file when making a library. If
-//! you are making an executable, the convention is to delete this file and
-//! start with main.zig instead.
+const c = @import("c");
+const mem = std.mem;
 const std = @import("std");
-const testing = std.testing;
 
-pub export fn add(a: i32, b: i32) i32 {
-    return a + b;
+pub fn example() !void {
+    var gpa: std.heap.DebugAllocator(.{}) = .init;
+    defer _ = gpa.deinit();
+
+    var arena: std.heap.ArenaAllocator = .init(gpa.allocator());
+    defer arena.deinit();
+
+    var cc: Context = try .init();
+    defer cc.deinit();
+
+    const src = (
+        \\export const html = () => `<p>Hello, world</p>`;
+        \\
+    );
+
+    try cc.register(arena.allocator(), "component", src);
+
+    const result = try cc.render(arena.allocator(), "component");
+    std.log.info("{s}", .{result});
 }
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+fn handleException(ctx: *c.JSContext) !noreturn {
+    const exception = c.JS_GetException(ctx);
+    defer c.JS_FreeValue(ctx, exception);
+
+    const str = c.JS_ToCString(ctx, exception);
+    defer c.JS_FreeCString(ctx, str);
+    const error_message = mem.span(str);
+
+    const stack = c.JS_GetPropertyStr(ctx, exception, "stack");
+    defer c.JS_FreeValue(ctx, stack);
+
+    const stack_str = c.JS_ToCString(ctx, stack);
+    defer c.JS_FreeCString(ctx, stack_str);
+    const stack_message = mem.span(stack_str);
+
+    std.log.err("JS Exception: {s} {s}", .{ error_message, stack_message });
+
+    return error.JSException;
 }
+
+const Context = struct {
+    rt: *c.JSRuntime,
+    ctx: *c.JSContext,
+
+    map: std.StringHashMapUnmanaged(Component),
+
+    pub const Component = struct {
+        mod: c.JSValue,
+    };
+
+    pub fn init() !Context {
+        const runtime = c.JS_NewRuntime() orelse return error.CannotAllocateJSRuntime;
+        errdefer c.JS_FreeRuntime(runtime);
+
+        const ctx = c.JS_NewContext(runtime) orelse return error.CannotAllocateJSContext;
+        errdefer c.JS_FreeContext(ctx);
+
+        return .{ .map = .empty, .rt = runtime, .ctx = ctx };
+    }
+
+    pub fn deinit(self: *Context) void {
+        var it = self.map.valueIterator();
+        while (it.next()) |component| {
+            c.JS_FreeValue(self.ctx, component.mod);
+        }
+        c.JS_FreeContext(self.ctx);
+        c.JS_FreeRuntime(self.rt);
+    }
+
+    pub fn register(self: *Context, arena: mem.Allocator, name: []const u8, src: []const u8) !void {
+        const key = try arena.dupe(u8, name);
+        const mod = c.JS_Eval(
+            self.ctx,
+            @ptrCast(src),
+            src.len,
+            @ptrCast(key),
+            c.JS_EVAL_TYPE_MODULE,
+        );
+        errdefer c.JS_FreeValue(self.ctx, mod);
+        switch (mod.tag) {
+            c.JS_TAG_EXCEPTION => try handleException(self.ctx),
+            else => {},
+        }
+
+        const result = try self.map.getOrPut(arena, key);
+
+        if (result.found_existing) return error.DuplicateComponentRegistered;
+
+        result.value_ptr.* = .{
+            .mod = mod,
+        };
+    }
+
+    pub fn render(self: *Context, arena: mem.Allocator, name: []const u8) ![]const u8 {
+        if (!self.map.contains(name)) return error.ComponentNotRegistered;
+
+        var buf: std.ArrayList(u8) = .init(arena);
+        defer buf.deinit();
+        try buf.writer().print(
+            \\import * as c from '{s}';
+            \\globalThis.html = c.html();
+        ,
+            .{name},
+        );
+
+        const t = try buf.toOwnedSliceSentinel(0);
+
+        const eval_result = c.JS_Eval(
+            self.ctx,
+            @ptrCast(t),
+            t.len,
+            "<input>",
+            c.JS_EVAL_TYPE_MODULE,
+        );
+        defer c.JS_FreeValue(self.ctx, eval_result);
+        switch (eval_result.tag) {
+            c.JS_TAG_EXCEPTION => try handleException(self.ctx),
+            else => {},
+        }
+
+        const global_object = c.JS_GetGlobalObject(self.ctx);
+        defer c.JS_FreeValue(self.ctx, global_object);
+
+        const html = c.JS_GetPropertyStr(self.ctx, global_object, "html");
+        defer c.JS_FreeValue(self.ctx, html);
+        switch (html.tag) {
+            c.JS_TAG_EXCEPTION => try handleException(self.ctx),
+            c.JS_TAG_STRING => {},
+            else => return error.UnexpectedValue,
+        }
+
+        const str = c.JS_ToCString(self.ctx, html);
+        defer c.JS_FreeCString(self.ctx, str);
+
+        const result: []const u8 = try arena.dupe(u8, mem.span(str));
+        return result;
+    }
+};
